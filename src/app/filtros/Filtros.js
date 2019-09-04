47d9748d66fc7a7c0a8bc8034d078cfed6edb43e
Kethrim <kethrim@ciencias.unam.mx>
2019-09-03 19:36:05 -0500

    Agregué varios cambios a los archivos js, css y html. Agregué los tres filtros (rojo,azul y verde) al archivo js. Modifiqué los valores RGB del css. Aagregué el js al html.

--- src/app/filtros/Filtros.js
+++ src/app/filtros/Filtros.js
@@ -1,4 +1,74 @@
-// Funcion que carga imagen (llamar desde el id 'imgOriginal', 'canvasOrig', 'canvasFilt')
-// Clase filtro
-// Filtro Rojo, Verde, azul
-// Funcion para guardar una imagen
+var filtro = ( function () {
+  var canvasOrig = document.getElementById('canvasOrig'),
+      canvasFilt = document.getElementById('canvasFilt'),
+      contextOrig = canvasOrig.getContext('2d'),
+      contextFilt = canvasFilt.getContext('2d'),
+      public = {};
+
+      /*
+      Método que carga una imagen.
+      */
+      public.cargarImagen = function (img) {
+        var imagen = new Image();
+        imagen.src = img;
+        imagen.onload = function () {
+          contextOrig.drawImage(imagen,0,0);
+          contextFilt.drawImage(imagen,0,0);
+        }
+      };
+
+      /*
+      Método para obtener los valores RGB de una imagen
+      */
+      public.obtenerDatosDeImagen = function () {
+        return contextOrig.getImageData(0,0, canvasOrig.width, canvasOrig.height);
+
+      };
+
+      /*
+      Método que aplica un filtro rojo a la imagen.
+      */
+      public.rojo = function () {
+        var datosDeImagen = filtro.obtenerDatosDeImagen(),
+            pixeles = datosDeImagen.data,
+            numPixeles = datosDeImagen.width * datosDeImagen.height;
+
+        for (var i = 0; i < numPixeles; i++) {
+          pixeles[(4*i)+1] = 0;
+          pixeles[(4*i)+2] = 0;
+        }
+        contextFilt.putImageData(datosDeImagen,0,0);
+      };
+
+      /*
+      Método que aplica un filtro azul a la imagen.
+      */
+      public.azul = function () {
+        var datosDeImagen = filtro.obtenerDatosDeImagen(),
+            pixeles = datosDeImagen.data,
+            numPixeles = datosDeImagen.width * datosDeImagen.height;
+
+        for (var i = 0; i < numPixeles; i++) {
+          pixeles[(4*i)] = 0;
+          pixeles[(4*i)+1] = 0;
+        }
+        contextFilt.putImageData(datosDeImagen,0,0);
+      };
+
+      /*
+      Método que aplica un filtro verde.
+      */
+      public.verde = function () {
+        var datosDeImagen = filtro.obtenerDatosDeImagen(),
+            pixeles = datosDeImagen.data,
+            numPixeles = datosDeImagen.width * datosDeImagen.height;
+
+        for (var i = 0; i < numPixeles; i++) {
+          pixeles[(4*i)] = 0;
+          pixeles[(4*i)+2] = 0;
+        }
+        contextFilt.putImageData(datosDeImagen,0,0);
+      };
+
+      return public;
+} ());
