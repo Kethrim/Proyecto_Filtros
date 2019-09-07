f77381f546c89fff004dd6104725754485ad5e0a
Kethrim <kethrim@ciencias.unam.mx>
2019-09-07 14:26:39 -0500

    Optimicé el código de los filtros, el filtro mosaico es en realidad el filtro en negativo.

--- src/app/filtros/Filtros.js
+++ src/app/filtros/Filtros.js
@@ -1,7 +1,9 @@
 let rojo = document.getElementById('filtroRojo'),
     azul = document.getElementById('filtroAzul'),
     verde = document.getElementById('filtroVerde'),
-    mosaico = document.getElementById('filtroMosaico');
+    mosaico = document.getElementById('filtroMosaico'),
+    guardar = document.getElementById('guardar');
+var imagenCargada;
 
 /**
 * Función que carga una imagen.
@@ -13,6 +15,7 @@ function cargarImagen(){
     lector.readAsDataURL(archivo);
     lector.onload = function(){
       leerImagen(lector.result);
+      imagenCargada = lector.result;
     }
   }
 }
@@ -53,18 +56,33 @@ function imgFiltrada() {
 }
 
 /**
+ *Función que cambia los valores rgb de la imagen.
+ */
+function aplicaFiltro(arrayPixeles, tam, rojo, verde, azul){
+  let r = rojo, a = azul, v = verde;
+  for (var i = 0; i < tam; i++) {
+   if (rojo!= 0) {
+     r = 2*arrayPixeles[4*i];
+   }else if (verde != 0) {
+     v = 2*arrayPixeles[(4*i)+1];
+   }else if (azul!= 0) {
+     a = 2*arrayPixeles[(4*i)+2];
+   }
+    arrayPixeles[4*i] = r - arrayPixeles[4*i];
+    arrayPixeles[(4*i)+1] = v - arrayPixeles[(4*i)+1];
+    arrayPixeles[(4*i)+2] =  a - arrayPixeles[(4*i)+2];
+  }
+}
+
+/**
 * Función que aplica el filtro rojo al darle click al botón "Filtro rojo".
 */
 rojo.onclick = function() {
   let arregloDePixeles = obtenerArregloDePixeles(),
       pixeles = arregloDePixeles.data,
       numPixeles = arregloDePixeles.width * arregloDePixeles.height;
-
-  for (var i = 0; i < numPixeles; i++) {
-    pixeles[(4*i)+1] = 0;
-    pixeles[(4*i)+2] = 0;
-  }
-  imgFiltrada().putImageData(arregloDePixeles,0,0);
+    aplicaFiltro(pixeles, numPixeles,1,0,0);
+    imgFiltrada().putImageData(arregloDePixeles,0,0);
 }
 
 /**
@@ -74,11 +92,7 @@ azul.onclick = function () {
   let arregloDePixeles = obtenerArregloDePixeles(),
       pixeles = arregloDePixeles.data,
       numPixeles = arregloDePixeles.width * arregloDePixeles.height;
-
-  for (var i = 0; i < numPixeles; i++) {
-    pixeles[(4*i)] = 0;
-    pixeles[(4*i)+1] = 0;
-  }
+      aplicaFiltro(pixeles, numPixeles,0,0,1);
   imgFiltrada().putImageData(arregloDePixeles,0,0);
 };
 
@@ -89,13 +103,29 @@ verde.onclick = function () {
   let arregloDePixeles = obtenerArregloDePixeles(),
       pixeles = arregloDePixeles.data,
       numPixeles = arregloDePixeles.width * arregloDePixeles.height;
-
-  for (var i = 0; i < numPixeles; i++) {
-    pixeles[(4*i)] = 0;
-    pixeles[(4*i)+2] = 0;
-  }
+      aplicaFiltro(pixeles, numPixeles,0,1,0);
   imgFiltrada().putImageData(arregloDePixeles,0,0);
 };
+
+guardar.onclick = function () {
+  //recibe un src
+  alert("Guardar imagen");
+  // window.newW = open(img);
+  // newW.document.execCommand("GuardarComo");
+  // newW.close();
+}
+
+mosaico.onclick = function () {
+  let arregloDePixeles = obtenerArregloDePixeles(),
+      pixeles = arregloDePixeles.data,
+      numPixeles = arregloDePixeles.width * arregloDePixeles.height;
+  for (var i = 0; i < numPixeles; i++) {
+    pixeles[4*i] = 255 - pixeles[4*i];
+    pixeles[4*i+1] = 255 - pixeles[4*i+1];
+    pixeles[4*i+2] = 255 - pixeles[4*i+2];
+  }
+  imgFiltrada().putImageData(arregloDePixeles,0,0);
+}
 
 
 /*
