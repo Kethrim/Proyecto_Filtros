7af89d8f5327b78be8cc4df1f6b2487f1970e2da
BrayanCaro <rojosuper@ciencias.unam.mx>
2019-09-07 21:13:07 -0500

    Anexé cosas al json

---
+++ pruebas/FiltrosPruebas/front.mochaTest.js
@@ -0,0 +1,9 @@
+//var app = require('../../src/app/filtros/Filtros.js'); // el archivo a probar
+
+var expect = require('chai').expect;
+
+describe('Prueba x', function () {
+  it ('Pasa 1', function () {
+    expect(0).to.equal(0);
+  });
+});
