287bb42397f00e7e51798bd43280d0cab6de7cc5
Kethrim <kethrim@ciencias.unam.mx>
2019-09-07 20:53:56 -0500

    Eliminé el botón de guardar imagen y el filtro mosaico

--- src/app/filtros/Filtros.js
+++ src/app/filtros/Filtros.js
@@ -1,9 +1,7 @@
 let rojo = document.getElementById('filtroRojo'),
     azul = document.getElementById('filtroAzul'),
     verde = document.getElementById('filtroVerde'),
-    mosaico = document.getElementById('filtroMosaico'),
-    guardar = document.getElementById('guardar');
-var imagenCargada;
+    mosaico = document.getElementById('filtroMosaico');
 
 /**
 * Función que carga una imagen.
@@ -63,9 +61,9 @@ function aplicaFiltro(arrayPixeles, tam, rojo, verde, azul){
   for (var i = 0; i < tam; i++) {
    if (rojo!= 0) {
      r = 2*arrayPixeles[4*i];
-   }else if (verde != 0) {
+   } if (verde != 0) {
      v = 2*arrayPixeles[(4*i)+1];
-   }else if (azul!= 0) {
+   } if (azul!= 0) {
      a = 2*arrayPixeles[(4*i)+2];
    }
     arrayPixeles[4*i] = r - arrayPixeles[4*i];
@@ -106,59 +104,3 @@ verde.onclick = function () {
       aplicaFiltro(pixeles, numPixeles,0,1,0);
   imgFiltrada().putImageData(arregloDePixeles,0,0);
 };
-
-guardar.onclick = function () {
-  //recibe un src
-  alert("Guardar imagen");
-  // window.newW = open(img);
-  // newW.document.execCommand("GuardarComo");
-  // newW.close();
-}
-
-mosaico.onclick = function () {
-  let arregloDePixeles = obtenerArregloDePixeles(),
-      pixeles = arregloDePixeles.data,
-      numPixeles = arregloDePixeles.width * arregloDePixeles.height;
-  for (var i = 0; i < numPixeles; i++) {
-    pixeles[4*i] = 255 - pixeles[4*i];
-    pixeles[4*i+1] = 255 - pixeles[4*i+1];
-    pixeles[4*i+2] = 255 - pixeles[4*i+2];
-  }
-  imgFiltrada().putImageData(arregloDePixeles,0,0);
-}
-
-
-/*
-      public.filtroM = function (arrayPixeles,promRojo, promAzul, promVerde, inicio, fin) {
-        console.log("El limite es "+fin);
-        for (var i = inicio; i < fin; i+=4) {
-          arrayPixeles[i] = promRojo;
-          arrayPixeles[i+1] = promVerde;
-          arrayPixeles[i+2] = promAzul;
-          //console.log("i "+i);
-        }
-        console.log("Color "+arrayPixeles[0]);
-      }
-
-      public.mosaico = function (tam) {
-        var datosDeImagen = filtro.obtenerDatosDeImagen(),
-            pixeles = datosDeImagen.data,
-            numPixeles = datosDeImagen.width * datosDeImagen.height,
-            limite = numPixeles/tam;
-
-            console.log("Limite " + limite);
-
-            for (var i = 0; i < numPixeles; i++) {
-              var rojo = 0, azul = 0, verde = 0;
-              for (var j = i; j < (limite+i); j++) {
-                rojo += pixeles[4*j];
-                verde += pixeles[(4*j)+1];
-                azul += pixeles[(4*j)+2];
-              }
-              var promRojo = rojo/limite, promAzul = azul/limite, promVerde = verde/limite;
-              console.log("Rojo "+promRojo+" azul "+promAzul+" verde "+promVerde);
-              filtro.filtroM(pixeles,promRojo, promAzul, promVerde,i,limite+i);
-            }
-            contextFilt.putImageData(datosDeImagen,0,0);
-      };
-*/
