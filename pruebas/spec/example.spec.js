e375a3d7b7088380861ffe0e9802afd562fd45db
BrayanCaro <rojosuper@ciencias.unam.mx>
2019-09-10 22:30:08 -0500

    Agregué una prueba La prueba funciona si se han cargado las imagenes

--- pruebas/spec/example.spec.js
+++ pruebas/spec/example.spec.js
@@ -1,10 +1,23 @@
-describe('Hola', () => {
-	it('Funciona El test solito', () => {
-		// arrange
+describe('Cargar imagenes', () => {
 
+	//beforeEach(function() {
+	//});
+
+	it(' Se cargaron los pixeles', () => {
+		// arrange
+		var archivo = new File(['img1'],
+							   'img1.jpg',
+							   {type:'image/jpg'});
+		cargarImagen(archivo);
+		
 		// act
+		let pixeles = obtenerArregloDePixeles().data;
 
 		// assert
-		expect(true).toBe(true);
+		expect(pixeles[0]).not.toBe(0);
+		expect(pixeles[1]).not.toBe(0);
+		expect(pixeles[2]).not.toBe(0);
+		expect(pixeles[3]).not.toBe(0);
 	});
+
 });
