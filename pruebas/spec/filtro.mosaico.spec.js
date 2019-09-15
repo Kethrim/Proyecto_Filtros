a8d23ffcbc1be1cb8ee4623d6cef350253c56392
BrayanCaro <rojosuper@ciencias.unam.mx>
2019-09-14 20:16:48 -0500

    agregué unas pruebas al filtro mosaico

--- pruebas/spec/filtro.mosaico.spec.js
+++ pruebas/spec/filtro.mosaico.spec.js
@@ -1,23 +1,35 @@
-xdescribe('Filtro mosaico pruebas', () => {
-	let arregloDePixeles = new Uint8ClampedArray(8);
-	it('usamos un pixel vacio', () => {
-		expect(arregloDePixeles).not.toBe(null);
-		console.log(arregloDePixeles);
-	});
-	
-	//it('probamos strings', () => {
-	//	
-	//	var a = "Hola";
-	//	
-	//	expect(a).not.toBe("Adios");
-	//});
-});
+/**
+ * Progama para probar el funcionamiento de la función aplica rojo
+ * 
+ * Simularemos tener un arreglo de pixeles y ejecutaremos el codigo para saber si se llenan correctamente
+ * 
+ * @author Trad Mateos Kethrim, Mártinez Santana Brayan
+ * @version 1
+ */
+describe('Filtro mosaico pruebas', () => {
+	let arregloDePixeles,
+		numPixeles;
 
-//describe('Holaaaaa', () => {
-//	//let a = new Uint8ClampedArray();
-//	
-//	it('probamos null', () => {
-//		
-//		expect(null).toBe(null);
-//	});
-//});
+	beforeEach(function () {
+		arregloDePixeles = new Uint8ClampedArray(400 * 4);
+		numPixeles = arregloDePixeles.length / 4;
+		// Llenamos el arreglo con colores al azar del 0 al 255
+		for (let i = 0; i < numPixeles; i++) {
+			arregloDePixeles[i * 4] = Math.trunc(Math.random() * (256));
+			arregloDePixeles[i * 4 + 1] = Math.trunc(Math.random() * (256));
+			arregloDePixeles[i * 4 + 2] = Math.trunc(Math.random() * (256));
+		}
+	});
+
+	it('no aplica el filtro cuando los parametros son negativos ni cuando ceros', () => {
+		expect(function () { aplicaFiltroMosaico(arregloDePixeles, 0, 0) }).toThrowError(RangeError);
+		expect(function () { aplicaFiltroMosaico(arregloDePixeles, -2, 12) }).toThrowError(RangeError);
+		expect(function () { aplicaFiltroMosaico(arregloDePixeles, 3, -2) }).toThrowError(RangeError);
+		expect(function () { aplicaFiltroMosaico(arregloDePixeles, -52, -23) }).toThrowError(RangeError);
+	});
+
+	it('no aplica el filtro cuando los parametros son muy grandes', () => {
+		expect(function () { aplicaFiltroMosaico(arregloDePixeles, 999, 999) }).toThrowError(RangeError);
+		expect(function () { aplicaFiltroMosaico(arregloDePixeles, 12, 600) }).toThrowError(RangeError);
+	});
+});
